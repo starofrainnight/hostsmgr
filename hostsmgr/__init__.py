48b1a4ae9cc12e0cfd2cbd497029ef00305b983c
Hong-She Liang <starofrainnight@gmail.com>
2018-04-19 11:34:52 +0800

    Bump version: 0.1.0 → 0.2.0

--- hostsmgr/__init__.py
+++ hostsmgr/__init__.py
@@ -6,4 +6,4 @@ from .hostsmgr import HostsMgr  # noqa
 
 __author__ = """Hong-She Liang"""
 __email__ = 'starofrainnight@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ tests_requires = [
 
 setup(
     name='hostsmgr',
-    version='0.1.0',
+    version='0.2.0',
     description="API for manage hosts file",
     long_description=long_description,
     author="Hong-She Liang",
