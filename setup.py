0582fd1fddbf045f5078b3f38a9f258d44106faf
Hong-She Liang <starofrainnight@gmail.com>
2018-04-19 17:19:03 +0800

    Added 'hosts' to keywords

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     install_requires=install_requires,
     license="Apache Software License",
     zip_safe=False,
-    keywords='hostsmgr',
+    keywords='hosts,hostsmgr',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
