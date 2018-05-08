a16f2158a4f0fbefa7f5be7b7fcfc1be541d6beb
Hong-She Liang <starofrainnight@gmail.com>
2018-05-08 10:26:39 +0800

    Bump version: 0.2.4 → 0.2.5

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ tests_requires = [
 
 setup(
     name='hostsmgr',
-    version='0.2.4',
+    version='0.2.5',
     description="API for manage hosts file",
     long_description=long_description,
     author="Hong-She Liang",
